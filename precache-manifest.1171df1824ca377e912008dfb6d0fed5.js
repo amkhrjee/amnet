9b7ff160fe80ab0d099bd937665a2d70a287674e
aniruddh-mukherjee <aniruddha.xccentric@gmail.com>
2020-07-21 18:43:15 +0530

    Updates

---
+++ precache-manifest.1171df1824ca377e912008dfb6d0fed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f203fe72e0e96375f2119379c8b0ea",
+    "url": "/amnet/index.html"
+  },
+  {
+    "revision": "bb7e91716945c45914fd",
+    "url": "/amnet/static/css/main.c45d9a9f.chunk.css"
+  },
+  {
+    "revision": "6081c09ce92eefa6c3b9",
+    "url": "/amnet/static/js/2.883cb37c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amnet/static/js/2.883cb37c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7e91716945c45914fd",
+    "url": "/amnet/static/js/main.e86da438.chunk.js"
+  },
+  {
+    "revision": "ef23b92af8850b4ee0c5",
+    "url": "/amnet/static/js/runtime-main.7125271a.js"
+  }
+]);
