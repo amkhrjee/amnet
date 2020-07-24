f1b491a4a4cbcfd953e3e3fbaf5b258d5689b9f9
aniruddh-mukherjee <aniruddha.xccentric@gmail.com>
2020-07-24 12:57:50 +0530

    workerjs

--- public/worker.js
+++ public/worker.js
@@ -1,49 +1,44 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = [ 'index.html', 'offline.html' ];
 
-var CACHE_NAME = 'pwa-task-manager';
-var urlsToCache = [
-  '/',
-  '/completed'
-];
+const self = this;
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
+                return cache.addAll(urlsToCache);
+            })
     )
-  );
 });
 
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
 });
