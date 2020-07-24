97afc0b25306eefd630684247fc04ed19ad0b1f8
aniruddh-mukherjee <aniruddha.xccentric@gmail.com>
2020-07-24 11:56:28 +0530

    Updates

---
+++ precache-manifest.66fbc334bcc8a8cfb6dd999a53aad977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e165380bb7f7beaf327d6a3dab98c9f7",
+    "url": "/amnet/index.html"
+  },
+  {
+    "revision": "f5d42160d5698b7e036b",
+    "url": "/amnet/static/css/main.f119ed89.chunk.css"
+  },
+  {
+    "revision": "b2ed8203cd47eefc6a63",
+    "url": "/amnet/static/js/2.62473e4f.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/amnet/static/js/2.62473e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d42160d5698b7e036b",
+    "url": "/amnet/static/js/main.530704e5.chunk.js"
+  },
+  {
+    "revision": "ef23b92af8850b4ee0c5",
+    "url": "/amnet/static/js/runtime-main.7125271a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e53a537d2f19d299c5a3f6b2bbea3695.js"
+  "/amnet/precache-manifest.66fbc334bcc8a8cfb6dd999a53aad977.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/amnet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
